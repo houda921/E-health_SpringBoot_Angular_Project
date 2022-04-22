09bc7909e0b9729c2c2fe27846a84f43219915e2
houda sayar <houdasayar12@gmail.com>
2022-04-22 16:20:41 +0200

    initial commit

---
+++ Health-care-system-SB-Angular/Angular-client-withSecurityAndLogin/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../service/login/login.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  form: any = {};
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private ls: LoginService ,private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.ls.register(this.form).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+        this.router.navigate(['login']);
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
+
+}
